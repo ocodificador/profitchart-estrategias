a9fdf9a43f2fdb938e134594e762b99588737bff
João Batista Silva <joao@linux.com>
2020-02-24 10:58:23 -0400

    Create DS_QI.pas
    The Q-Indicator (Trend Quality)
    Adaptação de código encontrado em:
    http://traders.com/Documentation/FEEDbk_docs
    /2004/04/TradersTips/TradersTips.html
    por João Batista Silva <joao@linux.com>

---
+++ Indicadores/DS_QI.pas
@@ -0,0 +1,71 @@
+input
+ FastAvgLen(7);
+ SlowAvgLen(15);
+ Trend_Period(4); 
+ Noise_Period(250); 
+ Correction(2);
+ AOTW_Limit(1.0); 
+var
+  Reversals : Float;
+  Periods   : Float;
+  DC        : Float;
+  CPC       : Float;
+  Trend     : Float;
+  DT        : Float;
+  Noise     : Float;
+  QIND      : Float;
+  Result    : Float;
+  Color     : Integer;
+begin
+  { Gera sinais de reversão com base na regra de cruzamento das médias }
+  Reversals := XAverage( CLOSE, FastAvgLen ) - XAverage( CLOSE, SlowAvgLen ) ;
+
+  Periods   := Sign( Reversals );
+
+  { Calcular preço menos preço anterior e calcular a soma acumulada ao longo de períodos dados por reversões }
+  DC        := CLOSE - CLOSE[1] ;
+  
+  if Periods <> Periods[1] then
+  begin
+    CPC   := 0;
+    Trend := 0;
+  end
+  else
+  begin
+    CPC   := CPC + DC * Periods ;
+   
+    { Para calcular a tendência, reduza o CPC por EMA de Trend_Period nos segmentos especificados (periods) }
+    Trend := CPC * 1 / Trend_Period + Trend[1] * ( 1 - ( 1 / Trend_Period ) ) ;
+  end;
+  
+  { Para calcular o ruído, subtraia a tendência do CPC, ajuste-a ao quadrado e suavise a danada }
+  DT    := CPC - Trend ;
+  Noise := Correction * Sqrt( Media( Noise_Period,  DT * DT ) );
+
+  { Para calcular o Q-Indicator divida a tendência pelo ruído obtido }
+  if Noise > 0 then QIND := Trend / Noise;
+  
+  if Abs(QIND) <= AOTW_Limit then Color := clGray
+  else if (Periods > 0) then Color := clGreen
+  else Color := clRed;
+
+  if (Color = clRed) and (Lowest(QIND,2) = QIND) then Color := clWhite;
+  if (Color = clGreen) and (Highest(QIND,2) > QIND) then Color := clWhite; 
+
+  if Periods < 0 then QIND := Neg(QIND);  
+  Plot(QIND);
+
+  PaintBar(Color);
+  
+end;
+
+
+
+
+
+
+
+
+
+
+
